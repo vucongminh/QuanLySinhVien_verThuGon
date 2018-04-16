4fa5db5f87f640ab48bf3653f6a738ad8eb7a4fb
huytung0404 <37171344+huytung0404@users.noreply.github.com>
2018-04-16 16:55:35 +0700

    dslop

--- TTCSDL_verThuGon(16-3)/QuanLySinhVien/formLop/frmDSLop.Designer.cs
+++ TTCSDL_verThuGon(16-3)/QuanLySinhVien/formLop/frmDSLop.Designer.cs
@@ -35,23 +35,29 @@
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
             this.MaLop = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.TenLop = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.TenLopTruong = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.TenGVCN = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txtTuKhoa = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Location = new System.Drawing.Point(12, 94);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(644, 252);
+            this.groupBox1.Size = new System.Drawing.Size(644, 294);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
@@ -63,7 +69,7 @@
             this.groupBox2.Controls.Add(this.btnThem);
             this.groupBox2.Location = new System.Drawing.Point(523, 11);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(115, 235);
+            this.groupBox2.Size = new System.Drawing.Size(115, 274);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             // 
@@ -74,7 +80,7 @@
             this.btnThoat.ForeColor = System.Drawing.Color.Red;
             this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
             this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThoat.Location = new System.Drawing.Point(6, 197);
+            this.btnThoat.Location = new System.Drawing.Point(6, 236);
             this.btnThoat.Name = "btnThoat";
             this.btnThoat.Size = new System.Drawing.Size(103, 32);
             this.btnThoat.TabIndex = 0;
@@ -89,7 +95,7 @@
             this.btnXoa.ForeColor = System.Drawing.Color.Blue;
             this.btnXoa.Image = ((System.Drawing.Image)(resources.GetObject("btnXoa.Image")));
             this.btnXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnXoa.Location = new System.Drawing.Point(6, 104);
+            this.btnXoa.Location = new System.Drawing.Point(6, 117);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(103, 32);
             this.btnXoa.TabIndex = 0;
@@ -104,7 +110,7 @@
             this.btnSua.ForeColor = System.Drawing.Color.Blue;
             this.btnSua.Image = ((System.Drawing.Image)(resources.GetObject("btnSua.Image")));
             this.btnSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSua.Location = new System.Drawing.Point(6, 56);
+            this.btnSua.Location = new System.Drawing.Point(6, 65);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(103, 32);
             this.btnSua.TabIndex = 0;
@@ -127,17 +133,6 @@
             this.btnThem.UseVisualStyleBackColor = false;
             this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Blue;
-            this.label2.Location = new System.Drawing.Point(12, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 19);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Danh Sách Lớp";
-            // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
@@ -149,7 +144,7 @@
             this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(4, 11);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(511, 235);
+            this.listView1.Size = new System.Drawing.Size(511, 277);
             this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -180,12 +175,85 @@
             this.TenGVCN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.TenGVCN.Width = 155;
             // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(92, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh Sách Lớp";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnTimKiem.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.ForeColor = System.Drawing.Color.Blue;
+            this.btnTimKiem.Image = ((System.Drawing.Image)(resources.GetObject("btnTimKiem.Image")));
+            this.btnTimKiem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTimKiem.Location = new System.Drawing.Point(543, 56);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(101, 32);
+            this.btnTimKiem.TabIndex = 13;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnTimKiem.UseVisualStyleBackColor = false;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Location = new System.Drawing.Point(331, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(196, 50);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tìm Kiếm Theo";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Blue;
+            this.radioButton2.Location = new System.Drawing.Point(98, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Tên Lớp";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Blue;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Mã Lớp";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // txtTuKhoa
+            // 
+            this.txtTuKhoa.Location = new System.Drawing.Point(331, 68);
+            this.txtTuKhoa.Name = "txtTuKhoa";
+            this.txtTuKhoa.Size = new System.Drawing.Size(196, 20);
+            this.txtTuKhoa.TabIndex = 12;
+            // 
             // frmDSLop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(668, 295);
+            this.ClientSize = new System.Drawing.Size(668, 391);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.txtTuKhoa);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -195,6 +263,8 @@
             this.Load += new System.EventHandler(this.frmDSLop_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,5 +283,10 @@
         public System.Windows.Forms.ColumnHeader MaLop;
         private System.Windows.Forms.ColumnHeader TenLopTruong;
         private System.Windows.Forms.ColumnHeader TenGVCN;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox txtTuKhoa;
     }
 }
--- TTCSDL_verThuGon(16-3)/QuanLySinhVien/formLop/frmDSLop.cs
+++ TTCSDL_verThuGon(16-3)/QuanLySinhVien/formLop/frmDSLop.cs
@@ -30,6 +30,22 @@ namespace QuanLySinhVien
             }
         }
 
+        protected void textBox1_Focus(Object sender, EventArgs e)
+        {
+            txtTuKhoa.Text = "";
+
+        }
+
+        public int KiemTra()
+        {
+            if (radioButton1.Checked == true)
+                return 1;
+            else if (radioButton2.Checked == true)
+                return 2;
+            else
+                return 0;
+        }
+
         private void frmDSLop_Load(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection();
@@ -37,6 +53,11 @@ namespace QuanLySinhVien
             con.Open();
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
+            this.txtTuKhoa.GotFocus += new EventHandler(textBox1_Focus); // enter event==get focus event 
+            this.txtTuKhoa.Text = "Ví Dụ: HTTT14 / Hệ Thống Thông Tin";
+
+            txtTuKhoa.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            txtTuKhoa.AutoCompleteSource = AutoCompleteSource.CustomSource;
             cmd.CommandText = "SELECT Lop.MaLop,TenLop,TenSV,TenGV FROM LOP ,SINHVIEN , GIAOVIEN  where LOP.MaLopTruong=SINHVIEN.MaSV and LOP.MaGVCN=GIAOVIEN.MaGV ";
             SqlDataReader rd;
             rd = cmd.ExecuteReader();
@@ -55,7 +76,68 @@ namespace QuanLySinhVien
 
         }
 
-       
+        private void getData(AutoCompleteStringCollection dataCollection)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            connetionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=QuanLySV24;Integrated Security=True";
+            string sql = "Select TenLop from LOP";
+
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                adapter.SelectCommand = command;
+                adapter.Fill(ds);
+                adapter.Dispose();
+                command.Dispose();
+                connection.Close();
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    dataCollection.Add(row[0].ToString());
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Khong co ket noi ! ");
+            }
+        }
+        private void getData2(AutoCompleteStringCollection dataCollection)
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            connetionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=QuanLySV24;Integrated Security=True";
+            string sql = "Select distinct MaLop from LOP  ";
+
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                adapter.SelectCommand = command;
+                adapter.Fill(ds);
+                adapter.Dispose();
+                command.Dispose();
+                connection.Close();
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    dataCollection.Add(row[0].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Khong co ket noi ! ");
+            }
+        }
+
         private void btnThoat_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -119,5 +201,107 @@ namespace QuanLySinhVien
                 MessageBox.Show("Hay Chon Sinh Vien Muon Sua !", "Thong Bao");
             }
         }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                listView1.Items[i].Remove();
+                i--;
+            }
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = KetNoi.str;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            if (txtTuKhoa.Text != " " && txtTuKhoa.Text != "Ví Dụ: HTTT14 / Hệ Thống Thông Tin")
+            {
+
+                if (KiemTra() == 1)
+                {
+                    cmd.CommandText = "Select distinct LOP.MaLop,TenLop,TenSV,TenGV from SINHVIEN, GIAOVIEN, LOP where LOP.MaLopTruong=SINHVIEN.MaSV and SINHVIEN.MaLop = LOP.MaLop and LOP.MaGVCN = GIAOVIEN.MaGV AND LOP.MaLop='" + txtTuKhoa.Text + "'";
+                    SqlDataReader rd;
+                    rd = cmd.ExecuteReader();
+
+                    DataTable td = new DataTable();
+                    td.Load(rd);
+
+                    if (td.Rows.Count != 0)
+                    {
+
+                        ListViewItem item = new ListViewItem(td.Rows[0][0].ToString());
+                        item.SubItems.Add(td.Rows[0][1].ToString());
+                        item.SubItems.Add(td.Rows[0][2].ToString());
+                        item.SubItems.Add(td.Rows[0][3].ToString());
+
+                        listView1.Items.Add(item);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không Tồn Tại Sinh Viên Có Mã " + txtTuKhoa.Text);
+                        frmDSLop_Load(sender, e);
+                    }
+                    this.txtTuKhoa.GotFocus += new EventHandler(textBox1_Focus); // enter event==get focus event 
+                    this.txtTuKhoa.Text = "Ví Dụ: HTTT14 / Hệ Thống Thông Tin";
+                }
+                else if (KiemTra() == 2)
+                {
+                    cmd.CommandText = "Select distinct LOP.MaLop,TenLop,TenSV,TenGV from SINHVIEN, GIAOVIEN, LOP where LOP.MaLopTruong=SINHVIEN.MaSV and SINHVIEN.MaLop = LOP.MaLop and LOP.MaGVCN = GIAOVIEN.MaGV and LOP.TenLop like N'%" + txtTuKhoa.Text + "%'";
+                    SqlDataReader rd;
+                    rd = cmd.ExecuteReader();
+
+                    DataTable td = new DataTable();
+                    td.Load(rd);
+
+                    if (td.Rows.Count != 0)
+                    {
+
+                        for (int i = 0; i < td.Rows.Count; i++)
+                        {
+                            ListViewItem item = new ListViewItem(td.Rows[i][0].ToString());
+                            item.SubItems.Add(td.Rows[i][1].ToString());
+                            item.SubItems.Add(td.Rows[i][2].ToString());
+                            item.SubItems.Add(td.Rows[i][3].ToString());
+
+                            listView1.Items.Add(item);
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không Tồn Tại Sinh Viên Có Tên " + txtTuKhoa.Text);
+                        frmDSLop_Load(sender, e);
+                    }
+                    //this.txtTuKhoa.GotFocus += new EventHandler(textBox1_Focus); // enter event==get focus event 
+                    //this.txtTuKhoa.Text = "Ví Dụ: SV0000 / Nguyễn Văn A";
+                }
+                else
+                {
+                    MessageBox.Show("Hãy Chọn Chức Năng Tìm Kiếm !");
+                    frmDSLop_Load(sender, e);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Hãy Nhập Từ Khóa !");
+                frmDSLop_Load(sender, e);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            AutoCompleteStringCollection DataCollection = new AutoCompleteStringCollection();
+            getData2(DataCollection);
+            txtTuKhoa.AutoCompleteCustomSource = DataCollection;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            AutoCompleteStringCollection DataCollection = new AutoCompleteStringCollection();
+            getData(DataCollection);
+            txtTuKhoa.AutoCompleteCustomSource = DataCollection;
+        }
     }
+    
 }
