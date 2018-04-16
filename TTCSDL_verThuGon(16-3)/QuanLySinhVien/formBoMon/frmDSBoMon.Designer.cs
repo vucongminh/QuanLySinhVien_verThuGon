849155703b2cd8eeaed0d7f00143c85bbcd03f06
huytung0404 <37171344+huytung0404@users.noreply.github.com>
2018-04-16 18:48:06 +0700

    r

--- TTCSDL_verThuGon(16-3)/QuanLySinhVien/formBoMon/frmDSBoMon.Designer.cs
+++ TTCSDL_verThuGon(16-3)/QuanLySinhVien/formBoMon/frmDSBoMon.Designer.cs
@@ -247,6 +247,7 @@
             // 
             // frmDSBoMon
             // 
+            this.AcceptButton = this.btnTimKiem;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
