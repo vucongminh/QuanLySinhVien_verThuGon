3fea9cbf73b9c9b57a4b6f9d548f10591bdd69fb
huytung0404 <37171344+huytung0404@users.noreply.github.com>
2018-04-23 13:23:01 +0700

    a

--- TTCSDL_verThuGon(16-3)/QuanLySinhVien/Properties/Settings.Designer.cs
+++ TTCSDL_verThuGon(16-3)/QuanLySinhVien/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QuanLySinhVien.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,17 @@ namespace QuanLySinhVien.Properties {
         public string Database1ConnectionString {
             get {
                 return ((string)(this["Database1ConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ADMIN-PC\\SQLEXPRESS;Initial Catalog=QuanLySV24;Integrated Security=Tr" +
+            "ue")]
+        public string QuanLySV24ConnectionString {
+            get {
+                return ((string)(this["QuanLySV24ConnectionString"]));
             }
         }
     }
